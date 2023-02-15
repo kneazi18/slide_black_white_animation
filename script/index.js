a19974f93e0554fffaa1394b56d941fbcbc1c04f
Furtuna <kneazi18@gmail.com>
2023-02-15 09:49:16 +0200

    este

--- script/index.js
+++ script/index.js
@@ -2,9 +2,7 @@ const container = document.querySelector('.container')
 const check = document.querySelector('.check-b')
 
 
-
 check.addEventListener('click',()=>{
     container.classList.toggle('whiteWite')
     container.classList.toggle('blackWite')
-
 })
