f049c3a19cdfdd130630b0210dbf029bbd50182a
Furtuna <kneazi18@gmail.com>
2023-02-15 09:27:05 +0200

    checkbox animation

---
+++ script/index.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('.container')
+const check = document.querySelector('.check-b')
+
+
+
+check.addEventListener('click',()=>{
+    container.classList.toggle('whiteWite')
+    container.classList.toggle('blackWite')
+
+})
